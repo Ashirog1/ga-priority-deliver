cb96fbd185882cc519a6799f0f9aaed172a2e33b
quocntn <quocntn@abivin.com>
2023-01-15 16:40:19 +0700

    add chromosome.h

--- include/chromosome.h
+++ include/chromosome.h
@@ -5,4 +5,13 @@
 #ifndef GA_PRIORITY_DELIVER_CHROMOSOME_H
 #define GA_PRIORITY_DELIVER_CHROMOSOME_H
 
+#include "vector"
+#include "customer.h"
+
+class Chromosome {
+public:
+    std::vector<Customer> gene;
+
+};
+
 #endif //GA_PRIORITY_DELIVER_CHROMOSOME_H
